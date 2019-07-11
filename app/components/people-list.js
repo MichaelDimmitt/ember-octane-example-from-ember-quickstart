254b581267f72e82d8248266abca426afbe1b3c3
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-10 23:50:10 -0400

    add an onclick event into the people-list component

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class PeopleListComponent extends Component {
+export default class PeopleList extends Component {
+  @action
+  showPerson(person) {
+    alert(person);
+  }
 }
