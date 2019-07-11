da52e0efe0aacc5d60a1cecf9ef4df6102d429b4
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-11 02:23:53 -0400

    add tracked properties to the application

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,9 +1,26 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class PeopleList extends Component {
   @action
   showPerson(person) {
     alert(person);
+  }
+  @action
+  updateLanguage(event) {
+    this.language = event.target.value;
+  }
+  @tracked language = 'en';
+
+  get greeting() {
+    switch (this.language) {
+      case 'en':
+        return 'Hello';
+      case 'de':
+        return 'Hallo';
+      case 'es':
+        return 'Hola';
+    }
   }
 }
